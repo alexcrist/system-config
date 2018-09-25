9e103cec95de63cadf2b7d2397d3b768e11f1566
alexcrist <alexecrist@gmail.com>
2018-09-25 11:38:35 -0400

    Add autoconfigure file

---
+++ autoconfigure_linux_mint.sh
@@ -0,0 +1,95 @@
+#!/bin/bash
+
+echo "========== GIT =========="
+
+echo "Installing git..."
+apt install git
+
+echo "Configuring git settings..."
+git config --global user.email "alexecrist@gmail.com"
+git config --global user.name "alexcrist"
+git config --global core.editor "vim"
+git config --global core.pager "cat"
+git config --global alias.co "checkout"
+git config --global alias.br "branch"
+git config --global alias.ci "commit"
+git config --global alias.st "status"
+git config --global alias.lg "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit -15"
+
+echo "========== VIM =========="
+
+echo "Installing vim..."
+apt install vim
+
+echo "Copying .vimrc..."
+cp .vimrc ~/.vimrc
+
+echo "Installing vundle package manager..."
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
+echo "Installing vim plugins..."
+vim +PluginInstall +qall
+
+echo "========== ZSH =========="
+
+echo "Installing zsh..."
+apt install zsh
+
+echo "Setting zsh to default shell..."
+chsh -s $(which zsh)
+
+echo "Installing oh-my-zsh package manager..."
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+echo "Installing zsh-autosuggestions..."
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+echo "Installing zsh theme..."
+git clone https://github.com/denysdovhan/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt"
+ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
+
+echo "Installing powerline fonts..."
+apt install fonts-powerline
+
+echo "========== NVM =========="
+
+echo "Installing nvm (0.33.11)..."
+git clone https://github.com/creationix/nvm.git ~/.nvm
+cd ~/.nvm
+git checkout v0.33.11
+. nvm.sh
+cd ..
+
+echo "========== ANACONDA =========="
+
+echo "Installing anaconda (5.2.0)..."
+cd /tmp
+curl -O https://repo.anaconda.com/archive/Anaconda3-5.2.0-Linux-x86_64.sh
+sha256sum Anaconda3-5.2.0-Linux-x86_64.sh
+bash Anaconda3-5.2.0-Linux-x86_64.sh
+
+echo "========== SPOTIFY =========="
+
+echo "Installing spotify..."
+apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 931FF8E79F0876134EDDBDCCA87FF9DF48BF1C90
+echo deb http://repository.spotify.com stable non-free | tee /etc/apt/sources.list.d/spotify.list
+apt update
+apt install spotify-client
+
+echo "========== CHROME =========="
+
+echo "Installing chrome..."
+wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add -
+sh -c 'echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
+apt udpate
+apt install google-chrome-stable
+
+echo "========== VSCODE =========="
+
+echo "Installing vscode..."
+curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
+install -o root -g root -m 644 microsoft.gpg /etc/apt/trusted.gpg.d/
+sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
+apt install apt-transport-https
+apt update
+apt install code
